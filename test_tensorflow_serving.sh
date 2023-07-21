18167a93e8d88c512d633057c3a7c9e73796e542
kevinchen21 <87917613+kevinchen21@users.noreply.github.com>
2023-07-21 15:24:20 -0500

    update Kubernetes setting

--- test_tensorflow_serving.sh
+++ test_tensorflow_serving.sh
@@ -7,6 +7,8 @@ target=/models/user_embedding_model \
   curl -d '{"instances": [[3.950000000000000178e+00,4.250000000000000000e+00,0.000000000000000000e+00,0.000000000000000000e+00,4.000000000000000000e+00,4.120000000000000107e+00,4.000000000000000000e+00,4.040000000000000036e+00,0.000000000000000000e+00,3.000000000000000000e+00,4.000000000000000000e+00,0.000000000000000000e+00,3.879999999999999893e+00,3.890000000000000124e+00]]}' \
   -X POST http://localhost:8501/v1/models/user_embedding_model:predict
 
+  curl -d '{"instances": [[3.950000000000000178e+00,4.250000000000000000e+00,0.000000000000000000e+00,0.000000000000000000e+00,4.000000000000000000e+00,4.120000000000000107e+00,4.000000000000000000e+00,4.040000000000000036e+00,0.000000000000000000e+00,3.000000000000000000e+00,4.000000000000000000e+00,0.000000000000000000e+00,3.879999999999999893e+00,3.890000000000000124e+00]]}' \
+  -X POST http://tensorflow-service/v1/models/user_embedding_model:predict
 
 
 docker cp /tmp/user_embedding_model serving_base:/models/user_embedding_model
@@ -18,7 +20,16 @@ docker run -p 8501:8501 -t user_embedding_model_serving &
 
 uvicorn server.main:app --host 127.0.0.1 --port 8000
 
-docker build -t movie_recommender_python:v1 .
+docker build -t movie_recommender_two_pod:1.0 .
+
+docker tag movie_recommender_one_pod:1.0 776746896215.dkr.ecr.us-west-2.amazonaws.com/movie-recommender-one-pod:1.0
+docker tag movie_recommender_two_pod:1.0 776746896215.dkr.ecr.us-west-2.amazonaws.com/movie-recommender-two-pod:1.0
+
+docker push 776746896215.dkr.ecr.us-west-2.amazonaws.com/movie-recommender-one-pod:1.0
+
+docker push 776746896215.dkr.ecr.us-west-2.amazonaws.com/user-embedding:1.0
+
+docker push 776746896215.dkr.ecr.us-west-2.amazonaws.com/movie-recommender-two-pod:1.0
 
 docker run --rm -p 80:80 movie_recommender_python:v1
 
@@ -27,4 +38,4 @@ docker inspect gracious_volhard | grep IPAddress
 
 docker-compose -f movieRec.yaml up
 
-79e9f9fd5a221f85dc5e34e6ecdcdaf9adc718bb7bdfbbb5d9125911bf819a05
+docker-compose -f latency_test_K8s.yaml up
